3bee77d05ab49781f8197194d8372a84b8c05ab0
Fan-Hal Koung <fanhal@corn10.stanford.edu>
2013-11-24 00:36:02 -0800

    added feature extraction

--- evalFunctions.py
+++ evalFunctions.py
@@ -10,6 +10,13 @@ def simpleEvaluation(state, evalArgs=None):
 		record[2]+=results[2]
 	return float(record[0])-record[1]+record[2]*.5
 
+def simpleEvaluation2(state, evalArgs=None):
+	totalStats = 0
+	for playerName in state.data.teams[0].team:
+		player = state.data.teams[0].playerMap.map[playerName]
+		totalStats += player.threes+player.reb+player.ast+player.pts+player.stl+player.blk-player.tov
+	return totalStats
+
 def extractFeatures(state):
 	features=[]
 	#Indicator on what positions each team has
@@ -21,4 +28,59 @@ def extractFeatures(state):
 		results = Team.play(state.data.teams[0], state.data.teams[opponent])
 		if results[0] > results[1]: features += [1]
 		else: features += [0]
+
+	return features
+
+def logLinearEvaluation(state, w):
+    """
+    Evaluate the current state using the log-linear evaluation
+    function.
+
+    @param state : Tuple of (game, player), the game is
+    a game object (see game.py for details, and player in
+    {'o', 'x'} designates whose turn it is.
+
+    @param w : List of feature weights.
+
+    @returns V : Evaluation of current game state.
+    """
+    # BEGIN_YOUR_CODE (around 4 lines of code expected)
+    features = extractFeatures(state)
+    product = 0
+    for i in range(len(w)):
+        product += w[i]*features[i]
+    return 1/(1+math.exp(-1*product))
+    # END_YOUR_CODE
+    return V
+
+def TDUpdate(state, nextState, reward, w, eta):
+    """
+    Given two sequential game states, updates the weights
+    with a step size of eta, using the Temporal Difference learning
+    algorithm.
+
+    @param state : Tuple of game state (game object, player).
+    @param nextState : Tuple of game state (game object, player),
+    note if the game is over this will be None. In this case, 
+    the next value for the TD update will be 0.
+    @param reward : The reward is 1 if the game is over and your
+    player won, 0 otherwise.
+    @param w : List of feature weights.
+    @param eta : Step size for learning.
+
+    @returns w : Updated weights.
+    """
+    # BEGIN_YOUR_CODE (around 13 lines of code expected)
+    if nextState is None:
+        r = reward-logLinearEvaluation(state,w)
+    else: r = reward+logLinearEvaluation(nextState,w)-logLinearEvaluation(state,w)
+    features = extractFeatures(state)
+    product = 0
+    for i in range(len(w)):
+        product += w[i]*features[i]
+    gradient = float(math.exp(-1*product))/math.pow(1+math.exp(-1*product),2)
+    for i in range(len(w)):
+        w[i] += eta*r*gradient*features[i]
+    # END_YOUR_CODE
+    return w
 	
--- run.py
+++ run.py
@@ -1,6 +1,56 @@
 from gamestate import *
 from util import *
 from agent import *
+
+def train(numPlayers,depth,numGames=2000):
+    alpha = 1e-1
+    numFeats = 11
+    evalFn = submission.logLinearEvaluation
+    w = [random.gauss(0,1e-2) for _ in range(numFeats)]
+    w[-1] = 0
+
+    for it in xrange(numGames):
+
+        g = GameState()
+        players=[RandomAgent() for i in xrange(numPlayers-1)]
+		players.insert(0,ABMinimaxAgent(evalFn,depth=depth,agent=0,evalArgs=))
+
+        over = False
+        playernum=0
+        while not over:
+            nextState=turn(players[playernum],playernum,g.clone())
+            w = submission.TDUpdate(g,nextState,0,w,alpha)
+            g = nextState
+            for p in players: p.setWeights(w)
+            over = g.is_over()
+
+        winner = g.winner()
+
+        if it%100 == 0:
+            print "Game : %d/%d"%(it,numGames)
+
+        # flip outcome for training
+        winner = 1.0-winner
+        w = submission.TDUpdate(state,None,winner,w,alpha)
+
+    # save weights
+    fid = open("weights.bin",'w')
+    import pickle
+    pickle.dump(w,fid)
+    fid.close()
+    return w
+
+def test(players,numGames=100,draw=False):
+    winners = [0,0]
+    for _ in xrange(numGames):
+        g = game.Game(game.LAYOUT)
+        winner = run_game(players,g,draw)
+        print "The winner is : Player %s"%players[winner].player
+        winners[winner]+=1
+    print "Summary:"
+    print "Player %s : %d/%d"%(players[0].player,winners[0],sum(winners))
+    print "Player %s : %d/%d"%(players[1].player,winners[1],sum(winners))
+
 def run_game(players,g):
 	over = False
 	while not over:
@@ -32,8 +82,8 @@ def main(args=None):
 	numPlayers = int(opts.numPlayers)
 	depth = int(opts.depth)
 
-	players=[ABMinimaxAgent('simpleEvaluation',depth=depth, agent=i+1) for i in xrange(numPlayers-1)]
-	players.insert(0,HumanAgent())
+	players=[RandomAgent() for i in xrange(numPlayers-1)]
+	players.insert(0,ABMinimaxAgent('simpleEvaluation2',depth=depth, agent=0))
 	print run_game(players,GameState(numPlayers=numPlayers))
 
 if __name__=="__main__":
